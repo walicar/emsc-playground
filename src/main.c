013407badf3f37b2290bf989ec6b22b05f936fe7
Will Alicar <will@alicar.me>
2024-02-05 18:45:08 -0800

    render square

--- src/main.c
+++ src/main.c
@@ -1,7 +1,8 @@
+#include <SDL/SDL.h>
+#include <emscripten.h>
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <emscripten.h>
 
 #define NUMTHRDS 4
 #define MAGNIFICATION 1e9
@@ -25,9 +26,7 @@ void *count_pi(void *arg)
     *sum += 1;
     pthread_mutex_unlock(&mutexsum);
 
-    EM_ASM_INT({
-        console.log('Thread ' + $0);
-    }, thread_id);
+    EM_ASM_INT({ console.log('Thread ' + $0); }, thread_id);
     printf("Thread %d: sum=%f\n", thread_id, *sum);
 
     pthread_exit((void *)0);
@@ -35,6 +34,7 @@ void *count_pi(void *arg)
 
 int main(int argc, char *argv[])
 {
+    printf("program started!\n");
     pthread_mutex_init(&mutexsum, NULL);
 
     pthread_attr_t attr;
@@ -63,11 +63,36 @@ int main(int argc, char *argv[])
     printf("Final Sum =  %f \n", *sum);
     free(sum);
 
-    EM_ASM({
-        console.log('Hello from C!');
-    });
+    EM_ASM({ console.log('Hello from C!'); });
 
     pthread_mutex_destroy(&mutexsum);
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_Surface *screen = SDL_SetVideoMode(256, 256, 32, SDL_SWSURFACE);
+
+    if (SDL_MUSTLOCK(screen))
+        SDL_LockSurface(screen);
+    for (int i = 0; i < 256; i++)
+    {
+        for (int j = 0; j < 256; j++)
+        {
+            // To emulate native behavior with blitting to screen, alpha component is
+            // ignored. Test that it is so by outputting data (and testing that it
+            // does get discarded)
+            int alpha = (i + j) % 255;
+            *((Uint32 *)screen->pixels + i * 256 + j) =
+                SDL_MapRGBA(screen->format, i, j, 255 - i, alpha);
+        }
+    }
+
+    if (SDL_MUSTLOCK(screen))
+        SDL_UnlockSurface(screen);
+    SDL_Flip(screen);
+
+    printf("you should see a smoothly-colored square - no sharp lines but the "
+           "square borders!\n");
+
+    SDL_Quit();
     pthread_exit(NULL);
 
     return 0;
